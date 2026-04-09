71f35b41051dfa2a43ac491f9bd6ac38360b9170
jeanie_lu1105 <45974321+jeanie-lu1105@users.noreply.github.com>
2026-04-09 12:43:19 +0800

    modify notes section

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,62 +1,65 @@
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import styled from "styled-components";
-import { ICONS } from "@/constants/icon.const";
+import {ICONS} from "@/constants/icon.const";
+
 const NavWrapper = styled.nav`
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+    background: white;
 
-  > ul {
-    display: flex;
-
-    > li {
-      width: 33.3333%;
-      text-align: center;
-      > a {
+    > ul {
         display: flex;
-        padding: 4px 0;
-        flex-direction: column;
-        align-items: center;
-        margin: 2px 0;
 
-        .icon {
-          width: 24px;
-          height: 24px;
-          fill: grey;
+        > li {
+            width: 33.3333%;
+            text-align: center;
+
+            > a {
+                display: flex;
+                padding: 4px 0;
+                flex-direction: column;
+                align-items: center;
+                margin: 2px 0;
+
+                .icon {
+                    width: 24px;
+                    height: 24px;
+                    fill: grey;
+                }
+
+                &.active-class {
+                    color: #007bff;
+
+                    .icon {
+                        fill: #007bff;
+                    }
+                }
+            }
         }
-
-        &.active-class {
-          color: #007bff;
-
-          .icon {
-            fill: #007bff;
-          }
-        }
-      }
     }
-  }
 `;
 
 const generateClassName = (isActive: boolean) => {
-  return isActive ? "active-class" : "inactive-class";
+    return isActive ? "active-class" : "inactive-class";
 };
 
 const Nav = () => {
-  return (
-    <NavWrapper>
-      <ul>
-        {ICONS.map((item, index) => (
-          <li key={index}>
-            <NavLink
-              to={item.to}
-              className={({ isActive }) => generateClassName(isActive)}
-            >
-              <item.icon className="icon" />
-              {item.displayName}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </NavWrapper>
-  );
+    return (
+        <NavWrapper>
+            <ul>
+                {ICONS.map((item, index) => (
+                    <li key={index}>
+                        <NavLink
+                            to={item.to}
+                            className={({isActive}) => generateClassName(isActive)}
+                        >
+                            <item.icon className="icon"/>
+                            {item.displayName}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+        </NavWrapper>
+    );
 };
 
 export default Nav;
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,82 +1,95 @@
 import Layout from "@/components/Layout";
-import { NumberPadOptions, TagList } from "@/constants/icon.const";
+import {NumberPadOptions, TagList} from "@/constants/icon.const";
 import styled from "styled-components";
 
 const TagsSection = styled.section`
-  background-color: #fff;
-  border: 1px solid red;
-  padding: 12px 16px;
-  > ol {
-    margin: 0 -12px;
+    background-color: #fff;
+    border: 1px solid red;
+    padding: 12px 16px;
 
-    > li {
-      background-color: #d9d9d9;
-      border-radius: 18px;
-      display: inline-block;
-      padding: 3px 18px;
-      font-size: 14px;
-      margin: 8px 12px;
+    > ol {
+        margin: 0 -12px;
+
+        > li {
+            background-color: #d9d9d9;
+            border-radius: 18px;
+            display: inline-block;
+            padding: 3px 18px;
+            font-size: 14px;
+            margin: 8px 12px;
+        }
     }
-  }
 
-  > button {
-    border: none;
-    background: none;
-    padding: 2px 4px;
-    color: #666;
-    border-bottom: 1px solid #333;
-    margin-top: 10px;
-  }
+    > button {
+        border: none;
+        background: none;
+        padding: 2px 4px;
+        color: #666;
+        border-bottom: 1px solid #333;
+        margin-top: 10px;
+    }
 `;
 const NotesSection = styled.section`
-  > label {
-    width: 100%;
-    background-color: #f6f6f6;
-    margin: 12px;
-    > span {
-      display: inline-block;
-      font-size: 14px;
-      color: #333;
-      margin-right: 12px;
+    padding: 10px 16px;
+    background-color: #f5f5f5;
+    font-size: 14px;
+
+    > label {
+        width: 100%;
+        align-items: center;
+        display: flex;
+
+        > span {
+            color: #333;
+            white-space: nowrap;
+            margin-right: 16px;
+        }
+
+        > input {
+            width: 100%;
+            display: block;
+            height: 72px;
+            background: none;
+            border: none;
+        }
     }
-  }
 `;
 const CategorySection = styled.section``;
 const NumberPadSection = styled.section``;
 
 function Money() {
-  return (
-    <Layout>
-      <TagsSection className="tags">
-        <ol>
-          {TagList.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ol>
-        <button>新增标签</button>
-      </TagsSection>
-      <NotesSection>
-        <label>
-          <span>备注</span>
-          <input type="text" />
-        </label>
-      </NotesSection>
-      <CategorySection>
-        <ul>
-          <li>支出</li>
-          <li>收入</li>
-        </ul>
-      </CategorySection>
-      <NumberPadSection>
-        <div>100</div>
-        <div>
-          {NumberPadOptions.map((option) => (
-            <button key={option}>{option}</button>
-          ))}
-        </div>
-      </NumberPadSection>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <TagsSection className="tags">
+                <ol>
+                    {TagList.map((tag) => (
+                        <li key={tag}>{tag}</li>
+                    ))}
+                </ol>
+                <button>新增标签</button>
+            </TagsSection>
+            <NotesSection>
+                <label>
+                    <span>备注</span>
+                    <input type="text" placeholder="在这里添加备注"/>
+                </label>
+            </NotesSection>
+            <CategorySection>
+                <ul>
+                    <li>支出</li>
+                    <li>收入</li>
+                </ul>
+            </CategorySection>
+            <NumberPadSection>
+                <div>100</div>
+                <div>
+                    {NumberPadOptions.map((option) => (
+                        <button key={option}>{option}</button>
+                    ))}
+                </div>
+            </NumberPadSection>
+        </Layout>
+    );
 }
 
 export default Money;
